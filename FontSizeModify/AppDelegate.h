04747b825817cb0666ade2b7665f55e9d794a715
“刘三火” <llp207@163.com>
2017-04-11 13:44:14 +0800

    全局根据不同的屏幕设置xib中字体的大小
    全局根据不同的屏幕设置xib中字体的大小

---
+++ FontSizeModify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FontSizeModify
+//
+//  Created by llp on 16/10/20.
+//  Copyright © 2016年 llp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FontSizeModify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FontSizeModify
+//
+//  Created by llp on 16/10/20.
+//  Copyright © 2016年 llp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
